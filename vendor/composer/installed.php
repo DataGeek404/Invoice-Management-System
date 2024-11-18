250c7a80de672c088d85a197a7dbf01cd2e4b557
DataGeek404 <techjayprivate@gmail.com>
2024-11-18 17:13:11 +0300

    Add composer.lock file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'data-geek/invoice-management-system',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8d8ee5406d75539b773a6292bf8a8903a7977ca9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'data-geek/invoice-management-system' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8d8ee5406d75539b773a6292bf8a8903a7977ca9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
